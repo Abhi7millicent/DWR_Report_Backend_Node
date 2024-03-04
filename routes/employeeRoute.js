e36b1075667fd8f6494e539f4b3cee43a098f637
abhishek@millicent.in <abhishek@millicent.in>
2024-03-04 16:14:35 +0530

    employee route rename

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,14 +1,16 @@
 import express from "express";
-import {
-  addEmployee,
-  getEmployeeList,
-} from "../controllers/employeeController.js";
+import { addEmployee, getEmployeeList, getEmployeeById, updateEmployee } from "../controllers/employeeController.js";
 
 const router = express.Router();
 
 // Define routes
 router.post("/add", addEmployee);
 
+
 router.get("/list", getEmployeeList);
+
+router.get('/:id', getEmployeeById);
+
+router.put('/:id', updateEmployee);
 
 export default router;

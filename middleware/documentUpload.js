efa73a6f7776e851d602a49a6b645aeafbe08922
abhishek@millicent.in <abhishek@millicent.in>
2024-03-15 16:55:18 +0530

    upload file

--- middleware/documentUpload.js
+++ middleware/documentUpload.js
@@ -1,9 +1,12 @@
 import path from "path";
+import dotenv from "dotenv";
 import multer from "multer";
 import { mkdirSync } from "fs";
+dotenv.config();
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "D:/DWR_Report_Backend_Node/uploads/documents"); // here its will be location file will be save
+    // cb(null, "D:/DWR_Report_Backend_Node/uploads/documents"); // here its will be location file will be save
+    cb(null, process.env.APP_ROUTE+"/uploads/documents"); // here its will be location file will be save
   },
   filename: (req, file, cb) => {
     let ext = path.extname(file.originalname);

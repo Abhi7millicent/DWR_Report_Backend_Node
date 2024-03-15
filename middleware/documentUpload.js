8cdb7647c4feec1da5e953329d7cea2cc4dd4b0a
abhishek@millicent.in <abhishek@millicent.in>
2024-03-15 17:22:10 +0530

    upload document

--- middleware/documentUpload.js
+++ middleware/documentUpload.js
@@ -6,7 +6,7 @@ dotenv.config();
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // cb(null, "D:/DWR_Report_Backend_Node/uploads/documents"); // here its will be location file will be save
-    cb(null, process.env.APP_ROUTE+"/uploads/documents"); // here its will be location file will be save
+    cb(null, process.env.APP_ROUTE+"/DWR_Report_Backend_Node/uploads/documents"); // here its will be location file will be save
   },
   filename: (req, file, cb) => {
     let ext = path.extname(file.originalname);
@@ -19,6 +19,8 @@ var uplaod = multer({
   storage: storage,
   fileFilter: (req, file, callback) => {
     if (file.mimetype == "image/png" || file.mimetype == "image/jpg") {
+      console.log( process.env.APP_ROUTE,"path");
+      console.log(__filename,"filename");
       callback(null, true);
     } else {
       console.log("only jpg and png file support!");

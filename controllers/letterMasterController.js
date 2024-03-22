94b9b8046050cfc05779f111f8a3a2e59430ce1b
abhishek@millicent.in <abhishek@millicent.in>
2024-03-22 14:52:03 +0530

    added changes

--- controllers/letterMasterController.js
+++ controllers/letterMasterController.js
@@ -14,7 +14,7 @@ export const addletterType = async (req, res) => {
 export const getletterType = async (req, res) => {
   try {
     const letter = await letterMaster.findAll();
-    res.json(200).json({ data: letter });
+    res.status(200).json({ data: letter });
   } catch (error) {
     console.error("Error getting letter:", error);
     res.status(500).json({ error: "Error getting letter" });

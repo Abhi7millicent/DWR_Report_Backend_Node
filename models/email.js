dc967e43a3231237bda5ecec8d9cd844a9982c58
abhishek@millicent.in <abhishek@millicent.in>
2024-03-28 14:35:56 +0530

    email update

--- models/email.js
+++ models/email.js
@@ -1,35 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../dataBase/sequelize.js";
 
-// const emailSchema = new mongoose.Schema({
-//   email: String,
-//   name: String,
-//   subject: String,
-//   body: String,
-//   sentAt: {
-//     type: String,
-//     default: function () {
-//       return new Date().toLocaleTimeString([], {
-//         hour: "2-digit",
-//         minute: "2-digit",
-//       });
-//     },
-//   },
-//   date: {
-//     type: String,
-//     default: function () {
-//       const now = new Date();
-//       const year = now.getFullYear();
-//       const month = (now.getMonth() + 1).toString().padStart(2, "0");
-//       const day = now.getDate().toString().padStart(2, "0");
-//       return `${year}-${month}-${day}`;
-//     },
-//   },
-
-//   file: String,
-// });
-
-// export const Email = mongoose.model("email_log_el", emailSchema);
 
 const Email = sequelize.define(
   "Email",

608db52fdb324996c6855eec6ed95aad4433c64b
abhishek@millicent.in <abhishek@millicent.in>
2024-03-29 11:34:50 +0530

    added changes

--- controllers/roleMasterController.js
+++ controllers/roleMasterController.js
@@ -1,25 +1,26 @@
 import roleMasterSchema from "../models/roleMaster.js";
 
 export const addRole = async (req, res) => {
-  try {
-      const { name } = req.body;
-      
-      // Check if role with the same name already exists
-      const existingRole = await roleMasterSchema.findOne({ name });
-
-      if (existingRole) {
-          // If role with the same name already exists, return a message
-          return res.status(400).json({ error: "Role already exists" });
-      }
-
-      // If role doesn't exist, create a new one
-      const role = await roleMasterSchema.create({ name });
-      res.status(200).json({ data: role });
-  } catch (error) {
-      console.error("Error adding role:", error);
-      res.status(500).json({ error: "Error adding role" });
-  }
-};
+    try {
+        const { name } = req.body;
+        
+        // Check if role with the same name already exists and deleteFlag is false
+        const existingRole = await roleMasterSchema.findOne({where:{ name: name, deleteFlag: false }});
+  
+        if (existingRole) {
+            // If role with the same name already exists and deleteFlag is false, return a message
+            return res.status(400).json({ error: "Role already exists" });
+        }
+  
+        // If role doesn't exist or deleteFlag is true, create a new one
+        const role = await roleMasterSchema.create({ name });
+        res.status(200).json({ data: role });
+    } catch (error) {
+        console.error("Error adding role:", error);
+        res.status(500).json({ error: "Error adding role" });
+    }
+  };
+  
 
 
 export const getRole = async (req, res) => {

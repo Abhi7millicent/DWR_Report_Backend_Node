2ca983cced755ab7edf63959ccea43342c18472b
abhishek@millicent.in <abhishek@millicent.in>
2024-04-03 17:47:54 +0530

    update

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -120,7 +120,7 @@ export const getListOfEvents = async (req, res) => {
     const anniversary = await getListOfAnniversary(startDate, endDate);
     const events = [...dateOfBirth, ...anniversary];
 
-    res.status(200).send(events);
+    res.status(200).send({data: events});
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
